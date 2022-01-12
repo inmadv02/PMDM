bf6212c17b34ba1fcf2653915eb6024f2d314bb8
María Inmaculada Domínguez Vargas <onemayn@outlook.es>
2022-01-12 17:59:49 +0100

    Act. 1 Done

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -45,16 +43,17 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Image.asset('assets/path_logo.png', width: 160),
+          const SizedBox(height: 60),
+          Image.asset('assets/path_logo.png', width: 190),
           const Text('Beautiful, Private Sharing',
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.white)),
-          const SizedBox(height: 60),
+          const SizedBox(height: 150),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               SizedBox(
-                  width: 200,
+                  width: 220,
                   height: 50,
                   child: ElevatedButton(
                     onPressed: () {},
@@ -64,31 +63,53 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     child: const Text('Sign In',
                         style: TextStyle(
+                            fontSize: 20,
                             color: Colors.deepOrangeAccent,
                             fontWeight: FontWeight.bold)),
                   )),
               const SizedBox(height: 30),
-              const Text('Alredy have a Path account?',
-                  style: TextStyle(color: Color.fromRGBO(238, 238, 238, 0.5))),
+              const SizedBox(
+                height: 24,
+                child: Text('Already have a Path account?',
+                    style:
+                        TextStyle(color: Color.fromRGBO(238, 238, 238, 0.5))),
+              ),
               SizedBox(
-                  width: 200,
+                  width: 220,
                   height: 50,
-                  child: ElevatedButton(
-                    onPressed: null,
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.grey),
-                    ),
-                    child: const Text('Log In'),
-                  )),
+                  child: OutlinedButton(
+                      onPressed: null,
+                      style: OutlinedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            side: BorderSide(
+                                color: Color.fromRGBO(212, 212, 212, 0.8),
+                                width: 1.0), // HERE
+                          ),
+                          side: const BorderSide(
+                              color: Color.fromRGBO(212, 212, 212, 0.8),
+                              width: 1.0)),
+                      child: const Text('Log In',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Color.fromRGBO(212, 212, 212, 0.5))))),
+              const SizedBox(height: 30),
               RichText(
+                textAlign: TextAlign.center,
                 text: const TextSpan(
                   text: 'By using Path, you agree to Path´s ',
                   children: <TextSpan>[
                     TextSpan(
                         text: 'Terms Of Use',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    TextSpan(text: ' world!'),
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 14)),
+                    TextSpan(text: ' and '),
+                    TextSpan(
+                        text: 'Privacy Policy',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 14)),
                   ],
                 ),
               )
