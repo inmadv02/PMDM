a65793e471622949b03dd31d7923b7ef2a419a62
María Inmaculada Domínguez Vargas <onemayn@outlook.es>
2022-01-13 22:15:08 +0100

    Card 2 completada

--- cards/lib/main.dart
+++ cards/lib/main.dart
@@ -12,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(backgroundColor: Colors.grey[600]),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -23,15 +21,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -39,68 +28,94 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(2.0)),
+            elevation: 4,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(children: <Widget>[
+                Row(
+                  children: [
+                    Container(
+                        margin: const EdgeInsets.only(right: 20.0),
+                        child: Text('El más barato, Buena puntuación',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.tealAccent[400]))),
+                    Container(
+                        margin: const EdgeInsets.only(left: 40.0),
+                        child: const Text('45 ofertas',
+                            style: TextStyle(color: Colors.grey)))
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Image.asset('assets/coche.png', width: 200),
+                Row(children: const [
+                  Text('Mini',
+                      textAlign: TextAlign.left, style: TextStyle(fontSize: 18))
+                ]),
+                const SizedBox(height: 7),
+                Row(children: const [
+                  Text('2-3 puertas · Citroen C1 Diesel o similar',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(color: Colors.grey))
+                ]),
+                const SizedBox(height: 7),
+                Row(
+                  children: const [
+                    Text('Man'),
+                    SizedBox(width: 15),
+                    Icon(
+                      Icons.ac_unit,
+                      size: 14,
+                    ),
+                    SizedBox(width: 6),
+                    Text('A/A'),
+                    SizedBox(width: 15),
+                    Icon(
+                      Icons.person,
+                      size: 17,
+                    ),
+                    SizedBox(width: 6),
+                    Text('4'),
+                    SizedBox(width: 17),
+                    Icon(
+                      Icons.card_travel,
+                      size: 14,
+                    ),
+                    SizedBox(width: 9),
+                    Text('1')
+                  ],
+                ),
+                const Divider(
+                  color: Colors.grey,
+                  thickness: 0.5,
+                ),
+                Row(children: [
+                  Container(
+                      margin: const EdgeInsets.only(right: 106.0),
+                      child: Text('35€',
+                          style: TextStyle(color: Colors.blue[900]))),
+                  Container(
+                      margin: const EdgeInsets.only(left: 106.0),
+                      child: Text('SELECCIONAR',
+                          style: TextStyle(color: Colors.blue[900])))
+                ])
+              ]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )),
     );
   }
 }
