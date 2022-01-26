5c861c0f8345e8abf4860083c1372328df1d2f84
María Inmaculada Domínguez Vargas <onemayn@outlook.es>
2022-01-26 16:50:01 +0100

    TheMovieDB flutter version

---
+++ moviedb_home_screen/lib/pages/home_page.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+import 'package:moviedb_home_screen/pages/movies_page.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    MoviesPage(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          toolbarHeight: 70,
+          title: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width * 1,
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(children: [
+                        RawMaterialButton(
+                          onPressed: () {},
+                          elevation: 2.0,
+                          fillColor: Colors.grey[200],
+                          child: Image.asset('assets/perfil.png', width: 45),
+                          shape: const CircleBorder(),
+                        ),
+                        const Text('Miguel Moreno',
+                            style: TextStyle(color: Colors.black, fontSize: 16))
+                      ]),
+                      RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 2.0,
+                        fillColor: Colors.grey[200],
+                        child: Icon(
+                          Icons.cast,
+                          size: 15.0,
+                          color: Colors.deepPurple[400],
+                        ),
+                        padding: const EdgeInsets.all(7.0),
+                        shape: const CircleBorder(),
+                      )
+                    ],
+                  )))),
+      body: SingleChildScrollView(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    child: const Text('Movie, Series, TV Shows',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 34, fontWeight: FontWeight.bold))))
+          ]),
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Padding(
+                padding: const EdgeInsets.only(left: 26.0),
+                child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.65,
+                    height: MediaQuery.of(context).size.height * 0.08,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[300],
+                          labelText: 'Search',
+                          border: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25.0)),
+                            borderSide: BorderSide(color: Colors.white),
+                          )),
+                    ))),
+            RawMaterialButton(
+              onPressed: () {},
+              elevation: 2.0,
+              fillColor: Colors.deepPurple[400],
+              child: const Icon(
+                Icons.tune,
+                size: 20.0,
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.all(14.0),
+              shape: const CircleBorder(),
+            )
+          ]),
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Padding(
+                padding: const EdgeInsets.fromLTRB(30, 30, 0, 10),
+                child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: const Text('Popular Movies',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold))))
+          ]),
+          _widgetOptions.elementAt(_selectedIndex)
+        ],
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ' '),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border), label: ' '),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.cloud_download_outlined), label: ' '),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications_none_sharp), label: ' ')
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ moviedb_home_screen/lib/pages/movies_page.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:moviedb_home_screen/models/movies_response.dart';
+
+class MoviesPage extends StatefulWidget {
+  const MoviesPage({Key? key}) : super(key: key);
+
+  @override
+  _MoviesPageState createState() => _MoviesPageState();
+}
+
+class _MoviesPageState extends State<MoviesPage> {
+  late Future<List<Movie>> items;
+
+  @override
+  void initState() {
+    items = fetchMovies();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: MediaQuery.of(context).size.height * 0.45,
+        child: FutureBuilder<List<Movie>>(
+          future: items,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return _movieList(snapshot.data!);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const Center(child: CircularProgressIndicator());
+          },
+        ));
+  }
+
+  Future<List<Movie>> fetchMovies() async {
+    final response = await http.get(Uri.parse(
+        'https://api.themoviedb.org/3/movie/popular?api_key=1c0ce60ed8e0cd9f90ffb9b539b4c646&language=en-US&page=1'));
+    if (response.statusCode == 200) {
+      return MoviesResponse.fromJson(jsonDecode(response.body)).results;
+    } else {
+      throw Exception('Failed to load the most popular movies');
+    }
+  }
+
+  Widget _movieList(List<Movie> movieList) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.4,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: movieList.length,
+        itemBuilder: (context, index) {
+          return _movieItem(movieList.elementAt(index), index);
+        },
+      ),
+    );
+  }
+
+  Widget _movieItem(Movie movie, int index) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(5, 15, 5, 15),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          debugPrint('Card tapped.');
+        },
+        child: SizedBox(
+            width: 200,
+            height: 200,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Image.network(
+                  'https://image.tmdb.org/t/p/w500/' + movie.posterPath,
+                  width: MediaQuery.of(context).size.width * 0.4),
+              Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(movie.title.toUpperCase(),
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))),
+            ])),
+      ),
+    );
+  }
+}
